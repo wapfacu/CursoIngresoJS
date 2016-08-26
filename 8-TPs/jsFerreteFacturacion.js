e10ac67751e894d50d7162446aece8d5ab25b357
wapfacu <wapfacu@gmail.com>
2016-08-25 21:33:17 -0300

    Ejercicio 1 - En clase - Primer controller y funciones.

--- 8-TPs/jsFerreteFacturacion.js
+++ 8-TPs/jsFerreteFacturacion.js
@@ -3,15 +3,37 @@ A.	Ingresar tres precios de productos y mostrar la suma de los mismos.
 B.	Ingresar tres precios de productos y mostrar el promedio de los mismos.
 C.	ingresar tres precios de productos  y mostrar precio final (más IVA 21%).
 */
-function Sumar () 
-{
-	
-}
-function Promedio () 
-{
-	
-}
-function PrecioFinal () 
-{
-	
-}
+
+var app = angular.module('ferreteFacturacion', []);
+
+app.controller("controladorFacturacion",function($scope){
+	$scope.test = "Hola Angular";
+
+$scope.datos={};
+	$scope.datos.precioUno="666";
+	$scope.datos.precioDos="213";
+	$scope.datos.precioTres="584";
+
+	$scope.sumar = function(){
+		var uno = parseInt($scope.datos.precioUno);
+		var dos = parseInt($scope.datos.precioDos);
+		var tres = parseInt($scope.datos.precioTres);
+
+		$scope.resultado = uno + dos + tres;
+		console.log($scope.resultado);
+	};
+
+	$scope.promedio = function(){
+		$scope.sumar();
+		$scope.promedio = $scope.resultado / parseInt("3");
+		console.log($scope.promedio);
+	};
+
+	$scope.preciofinal = function(){
+		$scope.sumar();
+		$scope.pfinal = $scope.resultado * 1.21;
+		console.log($scope.pfinal);
+	};
+});
+
+
